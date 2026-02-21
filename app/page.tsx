3362bd00e9fd770646b7bbeb854dd69eb4c78569
fadli208 <cimicimicilung4868@gmail.com>
2026-02-21 16:05:30 +0700

    update nav

--- app/page.tsx
+++ app/page.tsx
@@ -53,10 +53,10 @@ export default function LandingPage() {
   }, [])
 
   return (
-    <div className="flex min-h-screen flex-col bg-background relative overflow-hidden">
+    <div className="flex min-h-screen flex-col bg-background relative">
 
       {/* --- Latar Belakang Animasi Modern --- */}
-      <div className="absolute inset-0 z-0 pointer-events-none">
+      <div className="absolute inset-0 z-0 pointer-events-none overflow-hidden">
         {/* Efek grid futuristik */}
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f46e51a_1px,transparent_1px),linear-gradient(to_bottom,#4f46e51a_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_80%_80%_at_50%_0%,#000_70%,transparent_110%)]"></div>
         {/* Cahaya Biru bercahaya di tengah-atas */}
